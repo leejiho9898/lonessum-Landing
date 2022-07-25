ce3a4d5a3c427da3cf1b397a8fb6f4104d5c37fb
이지호 <cctv1998@naver.com>
2022-07-26 04:22:54 +0900

    design: Finish 컴포넌트 퍼블리싱

---
+++ components/Finish.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+import styled from "styled-components";
+import { Logo } from "../assets/img";
+import { Contents, GoAppLinkStyled, ImageWrapper } from "./Header";
+
+function Finish() {
+  return (
+    <FinishLayout>
+      <FinishContent>
+        <ImageWrapper>
+          <Image src={Logo} width={102} height={102} alt="외딴썸 로고" />
+        </ImageWrapper>
+        지금 외딴썸에서 <br /> 유학생 연인을 찾아보세요.
+        <GoAppLinkStyled> 매칭하러 가기</GoAppLinkStyled>
+      </FinishContent>
+    </FinishLayout>
+  );
+}
+
+const FinishLayout = styled.div`
+  background-color: #4adac4;
+  padding: 100px 0;
+  color: #ffff;
+`;
+const FinishContent = styled.div`
+  font-size: 28px;
+  font-weight: 700;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default Finish;
