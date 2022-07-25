c2d40c2b105ec2dd22c9dc097cd28911ab74ec1e
이지호 <cctv1998@naver.com>
2022-07-26 03:13:59 +0900

    feat: 필요한 이미지들 세팅

---
+++ assets/img/index.ts
@@ -0,0 +1,11 @@
+export { default as Instagram } from "./Instagram.png";
+export { default as KakaoTalk } from "./KakaoTalk.png";
+export { default as Logo } from "./Logo.png";
+export { default as StringLogo } from "./StringLogo.png";
+export { default as Model1 } from "./Model1.png";
+export { default as Model2 } from "./Model2.png";
+export { default as Demonstration1 } from "./Demonstration1.png";
+export { default as Demonstration2 } from "./Demonstration2.png";
+export { default as Demonstration3 } from "./Demonstration3.png";
+export { default as Demonstration4 } from "./Demonstration4.png";
+export { default as Demonstration5 } from "./Demonstration5.png";
